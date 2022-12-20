0e9b4549d89d3750b0dae37304f9876bb5eb8ff7
sun09vmfl <colin_first@naver.com>
2022-12-20 14:03:29 +0900

    미치겠다js..

--- js/index.js
+++ js/index.js
@@ -12,3 +12,14 @@ menuClose.addEventListener('click',()=>{
  menuPage.classList.add('none');
 })
 })();
+
+// 마우스 커서(흰색 원)
+document.addEventListener("mousemove", (e) => { 
+    const circle = document.querySelector(".circle")
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    circle.style.left = mouseX + 'px';
+    circle.style.top = mouseY + 'px';
+});
+
+// 마우스커서 안 보이게 (수정해야 함...) window.document.body.style.cursor = 'none';
