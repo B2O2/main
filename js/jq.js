75ffee081ea378fb5aca8fb89ce4713a98c9d209
sun09vmfl <colin_first@naver.com>
2022-12-21 17:51:34 +0900

    내맘대로되는게1도없음

--- js/jq.js
+++ js/jq.js
@@ -6,3 +6,29 @@ $(window).load(function () {
     $('#roading').fadeOut();
   }, 2000);
 });
+
+//휠 블락화 (한 페이지씩만 보이게 하는 것)
+window.addEventListener(
+  'wheel',
+  function (e) {
+    e.preventDefault();
+  },
+  { passive: false }
+);
+let mHtml = $('html');
+let page = 1;
+
+mHtml.animate({ scrollTop: 0 }, 10);
+
+$(window).on('wheel', function (e) {
+  if (mHtml.is(':animated')) return;
+  if (e.originalEvent.deltaY > 0) {
+    if (page == 13) return;
+    page++;
+  } else if (e.originalEvent.deltaY < 0) {
+    if (page == 1) return;
+    page--;
+  }
+  let posTop = (page - 1) * $(window).height();
+  mHtml.animate({ scrollTop: posTop });
+});
