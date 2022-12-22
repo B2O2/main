2a7073aea83770c1018c6f1711665d3ac7d47ed9
sun09vmfl <colin_first@naver.com>
2022-12-22 13:59:10 +0900

    1차 merge

--- js/index.js
+++ js/index.js
@@ -14,47 +14,70 @@
 })();
 
 //마우스 커서(반전 원/근데 한 페이지에서만 보임.. 왜?ㅠㅠ)
-const circle = document.querySelector('.circle');
-let mouseX = 0;
-let mouseY = 0;
-let circleX = 0;
-let circleY = 0;
-let speed = 0.4;
-const animate = () => {
-  let distX = mouseX - circleX;
-  let distY = mouseY - circleY;
-  let translatedValue = `translate3d(${circleX}px, ${circleY}px, 0 )`;
+(function cursor() {
+  const circle = window.document.body.querySelector('.circle');
+  let mouseX = 0;
+  let mouseY = 0;
+  let circleX = 0;
+  let circleY = 0;
+  let speed = 0.4;
+  const animate = () => {
+    let distX = mouseX - circleX;
+    let distY = mouseY - circleY;
+    let translatedValue = `translate3d(${circleX}px, ${circleY}px, 0 )`;
 
-  circleX = circleX + distX * speed;
-  circleY = circleY + distY * speed;
-  circle.style.transform = translatedValue;
+    circleX = circleX + distX * speed;
+    circleY = circleY + distY * speed;
+    circle.style.transform = translatedValue;
 
-  requestAnimationFrame(animate);
-};
-animate();
-window.addEventListener('mousemove', e => {
-  mouseX = e.clientX - circle.offsetWidth / 4;
-  mouseY = e.clientY - circle.offsetHeight / 4;
-});
+    requestAnimationFrame(animate);
+  };
+  animate();
+  window.addEventListener('mousemove', e => {
+    mouseX = e.clientX - circle.offsetWidth / 4;
+    mouseY = e.clientY - circle.offsetHeight / 4;
+  });
+})();
 
 //마우스커서 안 보이게 (수정해야 함...)
 //window.document.body.style.cursor = 'none';
 
 //가로스크롤
-const container = document.querySelector('.container');
-const stickyBox = document.querySelector('.sticky_box');
-const horizontal = document.querySelector('.horizontal');
-function calcHt(ht) {
-  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight;
-}
+(function Xscroll() {
+  const container = document.querySelector('.container');
+  const stickyBox = document.querySelector('.sticky_box');
+  const horizontal = document.querySelector('.horizontal');
+  function calcHt(ht) {
+    return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight;
+  }
 
-container.style.height = `${calcHt(horizontal)}px`; // 4775
-addEventListener('scroll', scrolled);
+  container.style.height = `${calcHt(horizontal)}px`; // 4775
+  addEventListener('scroll', scrolled);
 
-function scrolled() {
-  console.log('stickyBox.offsetTop:', stickyBox.offsetTop);
+  function scrolled() {
+    horizontal.style.transform = `translateX(-${stickyBox.offsetTop}px)`;
+  }
+})();
 
-  horizontal.style.transform = `translateX(-${stickyBox.offsetTop}px)`;
-}
+/*
+수정요소
+ 1. 주부생활 섹션 하단부 흰 바탕 보이는 것
+ 2. 커서 이벤트 메인에서만 작동하는 것
+ 3. 특정 위치 도달하면 푸터, 메인토글바, 페이지네이션 검정색으로 변경
+ 4. 특정 위치 도달하면 로고 블랙 버젼으로 변경되는 것.
+ 5. 스크롤 오류 해결 (내리면 하단으로 휘리릭 내려감)
+ 6. 스크롤바 없애기
+ 7. 로딩중 다른 창으로 못 넘어가게 블락하기
+ 8. 메인페이지 사진 확대하고 줄어드는 것 (중요)
+*/
 
-// 스크롤 내리면 검정색으로 변경
+// 검정색 만들기
+const footer = document.querySelector(`footer`);
+const section = document.querySelector(`.container`);
+
+let footerOffsetTop = footer.getClientRects().top;
+let clientHt = document.documentElement.clientHeight;
+
+console.log(footerOffsetTop); // 왜 언디파인드..?
+console.log(clientHt); // 959 현재 보여지는 화면의 길이
+console.log(footer.clientHeight); // footer의 길이 94
--- js/jq.js
+++ js/jq.js
@@ -23,7 +23,7 @@ mHtml.animate({ scrollTop: 0 }, 10);
 $(window).on('wheel', function (e) {
   if (mHtml.is(':animated')) return;
   if (e.originalEvent.deltaY > 0) {
-    if (page == 13) return;
+    if (page == 12) return;
     page++;
   } else if (e.originalEvent.deltaY < 0) {
     if (page == 1) return;
@@ -32,3 +32,37 @@ $(window).on('wheel', function (e) {
   let posTop = (page - 1) * $(window).height();
   mHtml.animate({ scrollTop: posTop });
 });
+
+//씨발
+let isVisible = false;
+const change = document.querySelector(`.container`);
+const black = document.querySelector(`footer`);
+
+$(window).on('scroll', function () {
+  if (checkVisible($('.container')) && !isVisible) {
+    footer.style.color = `#000`;
+    isVisible = true;
+  }
+});
+
+function checkVisible(elm, eval) {
+  eval = eval || 'object visible';
+  let viewportHeight = $(window).height(), // Viewport Height
+    scrolltop = $(window).scrollTop(), // Scroll Top
+    y = $(elm).offset().top,
+    elementHeight = $(elm).height();
+
+  if (eval == 'object visible') return y < viewportHeight + scrolltop && y > scrolltop - elementHeight;
+  if (eval == 'above') return y < viewportHeight + scrolltop;
+}
+
+// 시발? 이게아닌가봄
+// $(window).document.scroll(function () {
+//   let scrT = $(window).scrollTop();
+//   console.log(scrT); //스크롤 값 확인용
+//   if (scrT == $(document).height() - $(window).height()) {
+//     footer.style.color = `#000`;
+//   } else {
+//     footer.style.color = `#fff`;
+//   }
+// });
