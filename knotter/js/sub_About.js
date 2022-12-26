562201383d2879ca89b28a0dc220183d7f1a6ad1
gpwks8031 <gpwls8031@naver.com>
2022-12-26 15:12:33 +0900

    knotter 수정

--- knotter/js/sub_About.js
+++ knotter/js/sub_About.js
@@ -2,11 +2,7 @@ const logo = document.querySelector("header h1 .logo");
 const path1 = document.querySelector("header .menu_open svg #path1");
 const path2 = document.querySelector("header .menu_open svg #path2");
 const imgBg = document.querySelector(".img_bg");
-const on1 = document.querySelector(".text_container > .on1");
-const on2 = document.querySelector(".text_container > .on2");
-const on3 = document.querySelector(".text_container > .on3");
-const on4 = document.querySelector(".text_container > .on4");
-const on5 = document.querySelector(".text_container > .on5");
+const on = document.querySelectorAll(".text_container > .on");
 const arrow = document.querySelector(".text_container > .arrow");
 const span = document.querySelector(".specialities_wrap span");
 const active1 = document.querySelector(
@@ -32,72 +28,78 @@ const container2 = document.querySelector(".container2");
 const container3 = document.querySelector(".container3");
 const container4 = document.querySelector(".container4");
 
-// window.addEventListener("scroll", function (e) {
-//   e.preventDefault();
-//   scrollTo({
-//     top: getBox,
-//     behavior: "smooth",
-//   });
-// });
-
+// container2
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   console.log("scrollY", value);
 
+  // 글자 한 줄씩 슬라이드
   if (value > 300) {
     imgBg.style.animation = "movRight 0.25s ease-out";
-    on1.style.animation = "movLeft 0.25s forwards";
-    on2.style.animation = "movLeft 0.4s forwards";
-    on3.style.animation = "movLeft 0.55s forwards";
-    on4.style.animation = "movLeft 0.7s forwards";
-    on5.style.animation = "movLeft 0.85s forwards";
+    on[0].style.animation = "movLeft 0.25s forwards";
+    on[1].style.animation = "movLeft 0.4s forwards";
+    on[2].style.animation = "movLeft 0.55s forwards";
+    on[3].style.animation = "movLeft 0.7s forwards";
+    on[4].style.animation = "movLeft 0.85s forwards";
     arrow.style.animation = "fadeIn 0.95s forwards";
     mousewheelWrap.style.opacity = 0;
   } else {
     mousewheelWrap.style.opacity = 1;
   }
 
+  // span 굴러감
   if (value > 1680) {
-    span.style.animation = "move 2s forwards";
+    span.style.animation = "move 1.5s forwards";
     active1.style.animation = "active 1.5s forwards";
     active2.style.animation = "active 2.3s forwards";
     active3.style.animation = "active 5s forwards";
   }
 
-  if (value > 3900) {
-    container3Img.style.animation = "fadeIn 0.7s 0.4s forwards";
-    container3P.style.animation = "fadeIn 0.7s 0.8s forwards";
-  }
-});
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-
+  // 글자 opacity
   if (value > 2500) {
     clients.style.transition = "opacity 1s, transform 1s";
     clients.style.transform = "translate(0)";
     clients.style.opacity = 1;
   }
+});
 
-  if (value > 5600) {
-    line1.style.animation = "fadeIn 0.1s ease-in";
-    line1.style.opacity = 1;
-    line2.style.animation = "fadeIn 0.2s ease-in";
-    line2.style.opacity = 1;
-    line3.style.animation = "fadeIn 0.3s ease-in";
-    line3.style.opacity = 1;
-    line4.style.animation = "fadeIn 0.4s ease-in";
-    line4.style.opacity = 1;
-    eng.style.animation = "fadeIn 5s forwards";
+// container3
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  // 이미지와 글자 깜박임
+  if (value > 3900) {
+    container3Img.style.animation = "fadeIn 0.7s 0.4s forwards";
+    container3P.style.animation = "fadeIn 0.7s 0.8s forwards";
   }
 
+  // container4
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+
+    // 선들이 깜빡임
+    if (value >= 5460) {
+      line1.style.animation = "fadeIn 0.1s ease-in";
+      line1.style.opacity = 1;
+      line2.style.animation = "fadeIn 0.25s ease-in";
+      line2.style.opacity = 1;
+      line3.style.animation = "fadeIn 0.4s ease-in";
+      line3.style.opacity = 1;
+      line4.style.animation = "fadeIn 0.55s ease-in";
+      line4.style.opacity = 1;
+      eng.style.animation = "fadeIn 0.8s 0.8s forwards";
+    }
+  });
+});
+
+// logo와 toggle 메뉴 색 바뀜
+window.addEventListener("scroll", function () {
+  const path = document.querySelectorAll("path");
+  let value = window.scrollY;
   if (value >= 900) {
     logo.classList.add("color");
-    path1.classList.add("color");
-    path2.classList.add("color");
-  } else if (value <= 4270) {
-    logo.classList.remove("color");
-    path1.classList.remove("color");
-    path2.classList.remove("color");
+    path[0].classList.add("color");
+    path[1].classList.add("color");
   }
 });
 
